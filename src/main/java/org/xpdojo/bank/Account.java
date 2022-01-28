9faff9b4630ef2cd636eae6c3ae0a535021296ab
gauravchaudhary <gauravchaudhary8188@gmail.com>
2022-01-28 15:06:45 +0100

    deposit, withdraw, enable, disable, print functions implemented

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,63 @@
 package org.xpdojo.bank;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Account {
+    private double balance;
+    private boolean status;
+
+    public Account() {
+        balance = 0;
+        status = true;
+    }
+
+
+    public void deposit(double amount)
+    {
+        balance = balance + amount;
+    }
+
+    public boolean withdraw(double amount)
+    {
+        if(amount>=balance){
+            balance = balance - amount;
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    public void disable()
+    {
+        status = false;
+    }
+
+    public void enable()
+    {
+        status = true;
+    }
+
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public boolean printBalance()
+    {
+        if(status){
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Date date = new Date();
+            System.out.println("Your Account Balance is: "+balance+" Euros on "+dateFormat.format(date));
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -10,6 +10,44 @@ public class AccountTest {
     @Test
 	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account a = new Account();
+        a.deposit(100);
+
+        assertThat(a.getBalance()).isEqualTo(100);
+    }
+
+    public void withdrawAnAmountInsufficientBalance() {
+
+        Account a = new Account();
+        //a.deposit(100);
+
+
+        assertThat(a.withdraw(10)).isEqualTo(false);
+
+    }
+
+    public void withdrawAnAmountSufficientBalance() {
+
+        Account a = new Account();
+        a.deposit(100);
+
+
+        assertThat(a.withdraw(10)).isEqualTo(true);
+
+    }
+
+    public void displayClosedAccount () {
+
+        Account a = new Account();
+        a.disable();
+        assertThat(a.printBalance()).isEqualTo(false);
+
+    }
+
+    public void displayOpenedAccount () {
+
+        Account a = new Account();
+        assertThat(a.printBalance()).isEqualTo(true);
+
     }
 }
